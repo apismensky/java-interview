2398d6c6a5c35b6e78b7cb1e4248d0cb3fd01696
apismensky <alexey.pismenskiy@octanner.com>
2014-10-21 11:41:26 -0600

    simple list implementation

---
+++ src/main/java/com/octanner/lists/List.java
@@ -0,0 +1,178 @@
+package com.octanner.lists;
+
+/**
+ * Implementation of a linear singly-linked list.
+ * This list consists of a number of data elements in which each data element has a next pointer or next reference (the link) to the element that follows.
+ * The last element in the list has an empty or null link.
+ * Allows to store null elements
+ */
+public class List {
+
+    private Node head;
+    private int size;
+
+    private static class Node {
+        Node(Node linkedNode, Object value) {
+            this.linkedNode = linkedNode;
+            this.value = value;
+        }
+
+        Node linkedNode;
+        Object value;
+    }
+
+    /**
+     * Construct empty list
+     */
+    public List() {}
+
+    /**
+     * Construct list with given elements
+     * @param elements list elements
+     */
+    public List(Object... elements) {
+       for (Object element : elements) {
+           add(element);
+       }
+    }
+
+   /**
+     * Returns the element at the specified position in this list.
+     * @param index = index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
+    public Object get(int index) {
+        if (index < 0 || index > size - 1)
+            throw new IndexOutOfBoundsException("Illegal index value");
+        Node node = head;
+        for (int i = 0; i < size; i++) {
+           if (i == index) return node.value;
+           node = node.linkedNode;
+        }
+        return null;
+    }
+
+    /**
+     * Returns the number of elements in this list.
+     * @return the number of elements in this list
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Appends the specified element to the end of this list.
+     * @param value element to be appended to this list
+     * @return the instance of the list
+     */
+    public List add(Object value) {
+        head = new Node(head, value);
+        size++;
+        return this;
+    }
+
+    /**
+     * Removes the element at the specified position in this list (optional operation).
+     * Shifts any subsequent elements to the left (subtracts one from their indices).
+     * Returns the element that was removed from the list.
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
+    public Object remove(int index) {
+        if (index < 0 || index > size - 1)
+            throw new IndexOutOfBoundsException("Illegal index value");
+        Node prevNode = null;
+        Node thisNode = head;
+
+        for (int i = 0; i < size; i++) {
+            if (i == index) {
+                if (prevNode != null)
+                    prevNode.linkedNode = thisNode.linkedNode;
+                else
+                    head = thisNode.linkedNode;
+                size--;
+                return thisNode.value;
+            }
+            prevNode = thisNode;
+            thisNode = thisNode.linkedNode;
+        }
+        return null;
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+     * @param o element to search for
+     * @return the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element
+     */
+    public int indexOf(Object o) {
+        if (o == null) return -1;
+        Node node = head;
+        for (int i = 0; i < size; i++) {
+           if (node.value != null && node.value.equals(o)) {
+              return i;
+           }
+            node = node.linkedNode;
+        }
+        return -1;
+    }
+
+    /**
+     * String representation of the list
+     * Example: new List(1,2,3).toString() should produce {3,2,1}
+     * @return that represents the list
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        Node node = head;
+        for (int i = 0; i < size; i++) {
+            sb.append(node.value);
+            if (i != size - 1) {
+                sb.append(",");
+            }
+            node = node.linkedNode;
+        }
+        return sb.append("}").toString();
+    }
+
+    /**
+     * Implementation that simplifies testing
+     * Two lists are equals if they have the same number of elements and all elements are equals
+     * @param other list to compare
+     * @return true if lists are equals, false otherwise
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || (!(other instanceof List)))
+            return false;
+        List otherList = (List) other;
+        if (otherList.size != this.size)
+            return false;
+        for (int i = 0; i < size; i++) {
+            Object otherElement = otherList.get(i);
+            Object thisElement = this.get(i);
+            if (otherElement == null || thisElement == null)
+               return false;
+            if (!otherElement.equals(thisElement)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Hash Code implementation
+     * @todo This should not be used as an example
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        int hashCode = 0;
+        for (int i = 0; i < size; i++) {
+            if (this.get(i) != null) hashCode += this.get(i).hashCode();
+        }
+        return hashCode;
+    }
+}
---
+++ src/test/java/com/octanner/lists/ListTest.java
@@ -0,0 +1,236 @@
+package com.octanner.lists;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListTest {
+    /**
+     * Adding new elements to the list should increase its size
+     */
+    @Test
+    public void testAdd() {
+        List list = new List();
+        assertEquals(0, list.size());
+        list.add(7L).add(9L);
+        assertEquals(2, list.size());
+        assertEquals(new Long(9), list.get(0));
+        assertEquals(new Long(7), list.get(1));
+    }
+
+    /**
+     * Calling get method with negative argument should produce IndexOutOfBoundsException
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNegative() {
+        new List().get(-1);
+    }
+
+    /**
+     * Calling get method with index >= size() should produce IndexOutOfBoundsException
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetEmpty() {
+        new List().get(0);
+    }
+
+    /**
+     * Calling get method with index >= size() should produce IndexOutOfBoundsException
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetOutOfBound() {
+        new List(6L).get(1);
+    }
+
+    /**
+     * toString method on the empty list should produce {}
+     */
+    @Test
+    public void testToStringEmpty() {
+        assertEquals("{}", new List().toString());
+    }
+
+    /**
+     * toString method for List(7L) should produce {7}
+     */
+    @Test
+    public void testToStringOne() {
+        List list = new List(7L);
+        assertEquals(1, list.size());
+        assertEquals("{7}", list.toString());
+    }
+
+    /**
+     * toString method for list with multiple should work but the list values may be reversed
+     */
+    @Test
+    public void testToStringMany() {
+        List list = new List(7L,8L);
+        assertEquals(2, list.size());
+        assertEquals("{8,7}", list.toString());
+    }
+
+    /**
+     * Non null List is not equals to null
+     */
+    @Test
+    public void testEqualsNull() {
+        assertNotEquals(new List(), null);
+    }
+
+    /**
+     * A List instance is not equal to the object of the type other than list
+     */
+    @Test
+    public void testEqualsOtherType() {
+        assertNotEquals(new List(), "Foo");
+    }
+
+    /**
+     * Two empty lists are equals
+     */
+    @Test
+    public void testEqualsEmpty() {
+        assertEquals(new List(), new List());
+    }
+
+    /**
+     * Lists that don't have same elements but the same size are not equal
+     */
+    @Test
+    public void testEqualsFalse() {
+        assertNotEquals(new List(1, 2, 3), new List(2, 3, 4));
+    }
+
+    /**
+     * Lists that have same number of elements and all elements are equal should be equal
+     */
+    @Test
+    public void testEqualsTrue() {
+        assertEquals(new List(1, 2, 3), (new List(1, 2, 3)));
+    }
+
+    /**
+     * Lists with different number of elements are not equal
+     */
+    @Test
+    public void testEqualsDifferentNumber() {
+        assertNotEquals(new List(1, 2, 3, 3), new List(1, 2, 3));
+    }
+
+    /**
+     * Lists containing nulls are not equal
+     */
+    @Test
+    public void testEqualsWithNull() {
+        assertNotEquals(new List(1, null, 3), (new List(1, null, 3)));
+    }
+
+    /**
+     * If lists are equal their hashCode are equal
+     */
+    @Test
+    public void testHashCodeEquals() {
+        List first = new List(1, 2, 3);
+        List second = new List(1, 2, 3);
+        assertEquals(first, second);
+        assertEquals(first.hashCode(), second.hashCode());
+    }
+
+    /**
+     * Hash code method should return value !=0 even if one of the elements is null
+     */
+    @Test
+    public void testHashCodeWithNull() {
+        List list = new List(1, null, 3);
+        assertTrue(list.hashCode() != 0);
+    }
+
+    /**
+     * Removing elements with negative index argument should produce IndexOutOfBoundsException
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testDeleteNegative() {
+        new List().remove(-1);
+    }
+
+    /**
+     * Removing elements when index >= size() argument should produce IndexOutOfBoundsException
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testDeleteBiggerThanMaxIndex() {
+        new List().remove(1);
+    }
+
+    /**
+     * Removing the element from one-element list should return empty list
+     */
+    @Test
+    public void testDeleteOneElem() {
+        List list = new List(5L);
+        assertEquals(5L, list.remove(0));
+        assertEquals(0, list.size());
+    }
+
+    /**
+     * Removing element from the middle of the list should "shift" the remaining elements
+     */
+    @Test
+    public void testDeleteMiddle() {
+        List list = new List(5L,3L,1L,"Foo","Bar");
+        assertEquals("Foo", list.remove(1));
+        assertEquals(new List(5L,3L,1L,"Bar"), list);
+    }
+
+    /**
+     * Removing head element should work
+     */
+    @Test
+    public void testDeleteHead() {
+        List list = new List(5L,3L,1L,"Foo","Bar");
+        assertEquals("Bar", list.remove(0));
+        assertEquals(new List(5L,3L,1L,"Foo"), list);
+    }
+
+    /**
+     * Removing tail element should work
+     */
+    @Test
+    public void testDeleteTail() {
+        List list = new List(5L,3L,1L,"Foo","Bar");
+        assertEquals(5L, list.remove(4));
+        assertEquals(new List(3L,1L,"Foo","Bar"), list);
+    }
+
+    /**
+     * IndexOf null should return -1 even if the list contains null
+     */
+    @Test
+    public void testIndexOfNull() {
+        assertEquals(-1, new List(null, 1L).indexOf(null));
+    }
+
+    /**
+     * IndexOf in empty list should return -1
+     */
+    @Test
+    public void testIndexEmpty() {
+        assertEquals(-1, new List().indexOf(1L));
+    }
+
+    /**
+     * IndexOf should return correct index value if the element exists in the list
+     */
+    @Test
+    public void testIndexFound() {
+        assertEquals(1, new List(1,2,4).indexOf(2));
+    }
+
+    /**
+     * IndexOf should return -1 if the element does not exists in the list
+     */
+    @Test
+    public void testIndexNotFound() {
+        assertEquals(-1, new List(1, 2, 4).indexOf(7));
+    }
+}
