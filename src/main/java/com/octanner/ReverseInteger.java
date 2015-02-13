6d4d9ed2730cc88fd4f38bbb16893f816382163e
apismensky <alexey.pismenskiy@octanner.com>
2015-02-12 18:14:09 -0700

    reverse integer

---
+++ src/main/java/com/octanner/ReverseInteger.java
@@ -0,0 +1,15 @@
+package com.octanner;
+
+/**
+ * Reverse digits of an integer.
+ * <p/>
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ */
+public class ReverseInteger {
+    public static int reverse(int x) {
+        long abs = Long.parseLong(new StringBuilder(Long.toString(Math.abs((long) x))).reverse().toString());
+        if (abs > Integer.MAX_VALUE) abs = 0;
+        return  x < 0 ? -(int)abs : (int)abs;
+    }
+}
