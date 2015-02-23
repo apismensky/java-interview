6c1a1a2bc5a3324fa745e2c242d69f3062eab294
apismensky <alexey.pismenskiy@octanner.com>
2015-02-22 18:45:15 -0700

    Add task RomanToInteger

---
+++ src/main/java/com/octanner/RomanToInteger.java
@@ -0,0 +1,61 @@
+package com.octanner;
+
+
+/**
+ * Given a roman numeral, convert it to an integer.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ * I = 1
+ * V = 5
+ * X = 10
+ * L = 50
+ * C = 100
+ * D = 500
+ * M = 1000
+ */
+public class RomanToInteger {
+
+    private enum RomanLiteral {
+        I(1),
+        V(5),
+        X(10),
+        L(50),
+        C(100),
+        D(500),
+        M(1000);
+        private int v;
+        private RomanLiteral(int v) {
+            this.v = v;
+        }
+
+        static int getValue(char rl) {
+            for (RomanLiteral each: RomanLiteral.values()) {
+                if (each.name().equals("" + rl)) {
+                    return each.v;
+                }
+            }
+            throw new IllegalArgumentException("Illegal char " + rl);
+        }
+    }
+
+    public static int romanToInt(String s) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("Invalid Input: " + s);
+        }
+        int result = 0;
+        int i = 0;
+        while (i<s.length()) {
+            int value = RomanLiteral.getValue(s.charAt(i));
+            if (i<s.length()-1) {
+                int nextValue = RomanLiteral.getValue(s.charAt(i + 1));
+                if (nextValue > value) {
+                    value = nextValue - value;
+                    i++;
+                }
+            }
+            result += value;
+            i++;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/octanner/RomanToIntegerTest.java
@@ -0,0 +1,83 @@
+package com.octanner;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class RomanToIntegerTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNull() {
+        RomanToInteger.romanToInt(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmpty() {
+        RomanToInteger.romanToInt("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidCharacters() {
+        RomanToInteger.romanToInt("QQQ");
+    }
+
+    @Test
+    public void testI() {
+        assertEquals(1, RomanToInteger.romanToInt("I"));
+    }
+
+    @Test
+    public void testV() {
+        assertEquals(5, RomanToInteger.romanToInt("V"));
+    }
+
+    @Test
+    public void testX() {
+        assertEquals(10, RomanToInteger.romanToInt("X"));
+    }
+
+    @Test
+    public void testL() {
+        assertEquals(50, RomanToInteger.romanToInt("L"));
+    }
+
+    @Test
+    public void testC() {
+        assertEquals(100, RomanToInteger.romanToInt("C"));
+    }
+
+    @Test
+    public void testD() {
+        assertEquals(500, RomanToInteger.romanToInt("D"));
+    }
+
+    @Test
+    public void testM() {
+        assertEquals(1000, RomanToInteger.romanToInt("M"));
+    }
+
+    @Test
+    public void testII() {
+        assertEquals(2, RomanToInteger.romanToInt("II"));
+    }
+
+    @Test
+    public void testXV() {
+        assertEquals(15, RomanToInteger.romanToInt("XV"));
+    }
+
+    @Test
+    public void testIV() {
+        assertEquals(4, RomanToInteger.romanToInt("IV"));
+    }
+
+    @Test
+    public void testDC() {
+        assertEquals(600, RomanToInteger.romanToInt("DC"));
+    }
+
+    @Test
+    public void testMMMMCMXCIX(){
+        assertEquals(3999, RomanToInteger.romanToInt("MMMCMXCIX"));
+    }
+}
