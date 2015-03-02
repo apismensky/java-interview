cde45208d715443bad3da94540eebc3514b451fa
apismensky <alexey.pismenskiy@octanner.com>
2015-03-02 11:11:31 -0700

    Fix tests

--- src/main/java/com/octanner/MajorityElement.java
+++ src/main/java/com/octanner/MajorityElement.java
@@ -15,10 +15,11 @@ package com.octanner;
  If the counter is 0, we set the current candidate to e and we set the counter to 1.
  If the counter is not 0, we increment or decrement the counter according to whether e is the current candidate.
  When we are done, the current candidate is the majority element, if there is a majority.
-
  */
 public class MajorityElement {
     public static int majorityElement(int[] num) {
+        if (num == null || num.length==0)
+            throw new IllegalArgumentException("Argument can not be empty or null");
         int cand = num[0];
         int count = 1;
         for (int i = 1; i < num.length; i++) {
