b822d5597fb954d39292d633ae61f69e1de92289
apismensky <alexey.pismenskiy@octanner.com>
2015-03-02 11:04:46 -0700

    Каменты

--- src/main/java/com/octanner/MajorityElement.java
+++ src/main/java/com/octanner/MajorityElement.java
@@ -5,6 +5,17 @@ package com.octanner;
  * The majority element is the element that appears more than ⌊ n/2 ⌋ times.
  *
  * You may assume that the array is non-empty and the majority element always exist in the array.
+ *
+ * We will sweep down the sequence starting at the pointer position shown above.
+
+ As we sweep we maintain a pair consisting of a current candidate and a counter. Initially, the current candidate is unknown and the counter is 0.
+
+ When we move the pointer forward over an element e:
+
+ If the counter is 0, we set the current candidate to e and we set the counter to 1.
+ If the counter is not 0, we increment or decrement the counter according to whether e is the current candidate.
+ When we are done, the current candidate is the majority element, if there is a majority.
+
  */
 public class MajorityElement {
     public static int majorityElement(int[] num) {
