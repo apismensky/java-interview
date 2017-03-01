248b2267a0cb8d75ca1c35fc5c9601ea6224d54e
apismensky <alexey.pismenskiy@octanner.com>
2017-03-01 10:52:53 -0700

    Add leap year problem

---
+++ src/main/java/com/octanner/LeapYear.java
@@ -0,0 +1,21 @@
+package com.octanner;
+
+/**
+ * There is a leap year every year whose number is perfectly divisible by four -
+ * except for years which are both divisible by 100 and not divisible by 400.
+ * The second part of the rule effects century years.
+ * For example; the century years 1600 and 2000 are leap years,
+ * but the century years 1700, 1800, and 1900 are not.
+ */
+class LeapYear {
+
+    static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0 && !(year % 400 == 0))
+                return false;
+            else
+                return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/octanner/LeapYearTest.java
@@ -0,0 +1,38 @@
+package com.octanner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LeapYearTest {
+    @Test
+    public void test3() {
+        assertFalse(LeapYear.isLeapYear(3));
+    }
+    @Test
+    public void test4() {
+        assertTrue(LeapYear.isLeapYear(4));
+    }
+    @Test
+    public void test1600() {
+        assertTrue(LeapYear.isLeapYear(1600));
+    }
+    @Test
+    public void test1700() {
+        assertFalse(LeapYear.isLeapYear(1700));
+    }
+    @Test
+    public void test1800() {
+        assertFalse(LeapYear.isLeapYear(1800));
+    }
+    @Test
+    public void test1928() {
+        assertTrue(LeapYear.isLeapYear(1928));
+    }
+    @Test
+    public void test1950() {
+        assertFalse(LeapYear.isLeapYear(1950));
+    }
+
+}
